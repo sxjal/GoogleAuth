b58836b8e0cb398713b31db5869618744df83703
sajal <sajalsahu8@gmail.com>
2023-08-07 23:50:53 +0530

    front page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -21,15 +21,73 @@ class LoginScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
             Container(
-              margin: const EdgeInsets.only(top: 100),
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+              margin: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  const RotatedBox(
+                    quarterTurns: 3,
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 56,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: const Text(
+                          "A world of",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 227, 223, 223),
+                            fontSize: 36,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: const Text(
+                          "possibility in",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 227, 223, 223),
+                            fontSize: 36,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: const Text(
+                          "an app",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 227, 223, 223),
+                            fontSize: 36,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
