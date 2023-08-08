491aa19806fb17467dac31c531f16bae992c3ad2
sajal <sajalsahu8@gmail.com>
2023-08-08 21:15:07 +0530

    changes

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,92 +6,96 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 21, 50, 59),
-              Color.fromARGB(255, 60, 119, 121),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
+      body: Column(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 21, 50, 59),
+                  Color.fromARGB(255, 60, 119, 121),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
             ),
-            Container(
-              margin: const EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  const RotatedBox(
-                    quarterTurns: 3,
-                    child: Text(
-                      "Sign in",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 56,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  child: Row(
                     children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.1,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: const Text(
-                          "A world of",
-                          textAlign: TextAlign.start,
+                      const RotatedBox(
+                        quarterTurns: 3,
+                        child: Text(
+                          "Sign in",
                           style: TextStyle(
-                            color: Color.fromARGB(255, 227, 223, 223),
-                            fontSize: 36,
-                            fontWeight: FontWeight.w400,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 56,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: const Text(
-                          "possibility in",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 227, 223, 223),
-                            fontSize: 36,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.05,
                       ),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: const Text(
-                          "an app",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 227, 223, 223),
-                            fontSize: 36,
-                            fontWeight: FontWeight.w400,
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.1,
                           ),
-                        ),
+                          Container(
+                            margin: const EdgeInsets.all(5),
+                            child: const Text(
+                              "A world of",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 227, 223, 223),
+                                fontSize: 36,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.all(5),
+                            child: const Text(
+                              "possibility in",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 227, 223, 223),
+                                fontSize: 36,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.all(5),
+                            child: const Text(
+                              "an app",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 227, 223, 223),
+                                fontSize: 36,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
